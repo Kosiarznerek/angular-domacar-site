df88e04d9d28d169eda57391f1bf5d737a994b37
Kosiarznerek <dbabka.poczta@gmail.com>
2019-12-25 15:01:38 +0100

    Categories overlay

--- src/app/home/categories/categories.component.ts
+++ src/app/home/categories/categories.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-categories',
@@ -7,10 +7,48 @@ import {Component, OnInit} from '@angular/core';
 })
 export class CategoriesComponent implements OnInit {
 
+  // Component data
+  @ViewChild('backgroundContainer', {static: true}) backgroundContainer: ElementRef;
+  @ViewChild('backgroundOverlay', {static: true}) backgroundOverlay: ElementRef;
+
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  /**
+   * On window scroll
+   * @param event Event object
+   */
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event?: Event): void {
+
+    const nativeElement: HTMLElement = this.backgroundContainer.nativeElement;
+    const rect: ClientRect | DOMRect = nativeElement.getBoundingClientRect();
+    let screenTop: number = rect.top - window.innerHeight <= 0
+      ? Math.abs(rect.top - window.innerHeight)
+      : 0;
+    screenTop = screenTop > window.innerHeight + rect.height
+      ? window.innerHeight + rect.height
+      : screenTop;
+    const translate: number = this._rescale(screenTop, 0, window.innerHeight + rect.height, -100, 100);
+    this.backgroundOverlay.nativeElement.style.transform = `translate3d(0px, ${translate}px, 0px)`;
+
+  }
+
+  /**
+   * Rescales number to new range
+   * @param num Number to scale
+   * @param inMin Input scale min
+   * @param inMax Input scale max
+   * @param outMin Output scale min
+   * @param outMax Output scale max
+   */
+  private _rescale(num: number, inMin: number, inMax: number, outMin: number, outMax: number): number {
+
+    return (num - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+
   }
 
 }
