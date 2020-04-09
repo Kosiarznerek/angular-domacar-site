374ddaa79f20caa7159ca6cc1b50c10e91390fdf
Kosiarzenerek <dbabka.poczta@gmail.com>
2020-04-09 18:02:55 +0200

    Base shop product preview module structure

--- src/app/shop/product-preview/product-preview.component.ts
+++ src/app/shop/product-preview/product-preview.component.ts
@@ -1,28 +1,15 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {map, take} from 'rxjs/operators';
-import {EShopCategory} from '../../cart-store.service.models';
 
 @Component({
-  selector: 'app-product-preview',
   templateUrl: './product-preview.component.html',
   styleUrls: ['./product-preview.component.scss']
 })
 export class ProductPreviewComponent implements OnInit {
 
-  constructor(
-    private readonly _activatedRoute: ActivatedRoute,
-  ) {
+  constructor() {
   }
 
-  public async ngOnInit(): Promise<void> {
-
-    // Extract shop category
-    const shopCategory: EShopCategory = await this._activatedRoute.parent.parent.url.pipe(
-      map(v => v[0].path as EShopCategory),
-      take(1)
-    ).toPromise();
-
+  ngOnInit(): void {
   }
 
 }
--- src/app/shop/product-preview/product-preview.module.ts
+++ src/app/shop/product-preview/product-preview.module.ts
@@ -2,10 +2,18 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ProductPreviewComponent} from './product-preview.component';
 import {RouterModule} from '@angular/router';
+import {GalleryComponent} from './gallery/gallery.component';
+import {GeneralComponent} from './general/general.component';
+import {DetailsComponent} from './details/details.component';
+import {RelatedProductsComponent} from './related-products/related-products.component';
 
 @NgModule({
   declarations: [
     ProductPreviewComponent,
+    GalleryComponent,
+    GeneralComponent,
+    DetailsComponent,
+    RelatedProductsComponent,
   ],
   imports: [
     CommonModule,
